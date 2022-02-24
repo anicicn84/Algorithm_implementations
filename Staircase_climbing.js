80f4583376c524d408531ce192587d3d82d8b009
Nikola Aničić <anicicn@yahoo.com>
2022-02-24 20:18:15 +0100

    Update Staircase_climbing.js
    typo fix and some other minor stuff in the comments

--- Staircase_climbing.js
+++ Staircase_climbing.js
@@ -4,12 +4,12 @@
 // of ways in which you can climb the staircase. 
 // For example, if you were given a staircase of height = 3 and
 // maxSteps = 2 you could climb the staircase in 3 ways. You could
-// take 1 step, 1 step then 1 step, you could also take 1 step, then 
+// take 1 step, 1 step, then 1 step, you could also take 1 step, then 
 // 2 steps, and you could take 2 steps, then 1 step. 
 // Note that maxSteps <= height will always be true.
 
 // Sample input: height = 4, maxSteps = 2
-// Sample output is 5:
+// Sample output is 5, here are the possible ways:
 // 1, 1, 1, 1
 // 1, 1, 2
 // 1, 2, 1
@@ -20,15 +20,14 @@ function staircaseTraversal(height, maxSteps) {
   // sliding window solution
 	let currentNumberOfWays = 0;
 	const waysToTop = [1];
-	// <= height is to make sure we are including the hight
+	// <= height is to make sure we are including the height
 	for (let currentHeight = 1; currentHeight <= height; currentHeight++) {
-		// we need to calculate start & end of our window
-		// start of the prev window so that we can substract that value
+		// start of the previous window so that we can substract that value
 		let startOfPreviousWindow = currentHeight - maxSteps - 1;
 		let endOfWindow = currentHeight - 1; //we want to look at the previous value from this height
-		if (startOfPreviousWindow >= 0) { // it is not in the negative, left of 0
+		if (startOfPreviousWindow >= 0) { // it is not in the negative area, left of 0
 			currentNumberOfWays -= waysToTop[startOfPreviousWindow]; // remove the element from the left
-																															// which got dropped out of the window
+																															// which got dropped fromt he left of the window
 			// only then we can remove something from the left, otherwise nothing to remove
 		}
 		currentNumberOfWays += waysToTop[endOfWindow]; // the new value added from the right
