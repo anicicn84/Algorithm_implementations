7a8cde2e4972f4fa41ea59180f7b169cdc9f8dc1
Nikola Aničić <anicicn@yahoo.com>
2022-02-24 11:08:09 +0100

    Create bst_traversal_recursive.js
    BST traversal recursive

---
+++ bst_traversal_recursive.js
@@ -0,0 +1,34 @@
+function inOrderTraverse(tree, array) {
+	if (tree !== null){
+		inOrderTraverse(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverse(tree.right, array);
+	}
+	
+	return array;
+}
+
+function preOrderTraverse(tree, array) {
+  if (tree !== null){
+		array.push(tree.value);
+		preOrderTraverse(tree.left, array);
+		preOrderTraverse(tree.right, array);
+	}
+	
+	return array;
+}
+
+function postOrderTraverse(tree, array) {
+  if (tree !== null){
+		postOrderTraverse(tree.left, array);
+		postOrderTraverse(tree.right, array);
+		array.push(tree.value);
+	}
+	
+	return array;
+}
+
+// Do not edit the lines below.
+exports.inOrderTraverse = inOrderTraverse;
+exports.preOrderTraverse = preOrderTraverse;
+exports.postOrderTraverse = postOrderTraverse;
